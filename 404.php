5443c17c1605082746eeaf4f50c44b5d127431b3
mk2011 <root@mkrss.com>
2012-11-30 22:16:34 +0800

    modified:   404.php

--- 404.php
+++ 404.php
@@ -1,3 +1,8 @@
 <?php
-echo '404';
+if (strstr($_SERVER['REQUEST_URI'],'index.php')){
+    header('HTTP/1.0 404 Not Found');
+    echo "<h1>404 Not Found</h1>";
+    echo "The page that you have requested could not be found.";
+    exit();
+}
 ?>
