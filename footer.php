e41572e7831f0250239a75b223681e108b07f345
mk <mk@mk.local>
2012-11-30 21:56:28 +0800

    Committer: mk <mk@mk.local> new branch onecolumn

--- footer.php
+++
@@ -1,4 +0,0 @@
-<div id="layout-footer">
-	<span> Copyright © 2012-2012 mk &nbsp&nbsp&nbsp <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds.</span> <a href="feed://b.oozk.com/index.php/feed" class="home rss">subscribe via rss</a>
-</div>
-
